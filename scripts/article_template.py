22e40ef4a9868929773570f4782950900e468013
xiaoxiae <tomas.slama.131@gmail.com>
2019-11-01 00:41:48 +0100

    update artice_template script

--- scripts/article_template.py
+++ scripts/article_template.py
@@ -1,36 +1,30 @@
 import datetime
+import unidecode
+import os
 
-clean_dict = {"á": "a",
-              "č": "c",
-              "ď": "d",
-              "é": "e",
-              "ě": "e",
-              "í": "i",
-              "ň": "n",
-              "ó": "o",
-              "ř": "r",
-              "š": "s",
-              "ť": "t",
-              "ý": "y",
-              "ž": "z",
-              " ": "-"}
 date = datetime.date.today().strftime("%Y-%m-%d")
 
-
-def make_file_name(text):
-    text = text.lower()
-    for i in range(len(text)):
-        if text[i] in clean_dict.keys():
-            text = text[:i] + clean_dict[text[i]] + text[i + 1:]
-    text = date + "-" + text + ".md"
-    return text
-
-
 print("Welcome to MEZIKLASI article template generator")
-name = input("Article tittle: ")
-category = input("Article category: ")
-header = '---\nlayout: post\ntittle: "' + name + '"\ndate: ' + date + '\ncategory: ' + category + '\n---\n\n\n'
 
-file = open("../_drafts/" + make_file_name(name), "w")
-file.write(header)
-file.close() 
+name = input("Article title: ")
+category = input("Article category: ")
+
+header = f"""---
+layout: post
+title: "{name}"
+date: {date}
+category: {category}
+---
+
+
+"""
+
+drafts_folder = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "..",
+    "_drafts",
+    f"{date}-{unidecode.unidecode(name.replace(' ', '-'))}",
+)
+
+with open(drafts_folder, "w") as file:
+    file.write(header)
